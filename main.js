16454790be752cda4ad97d5b60a9092678b2c21b
ceq5032 <125389208+ceq5032@users.noreply.github.com>
2024-11-07 12:21:02 -0500

    Updates

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-
 import * as THREE from 'https://cdn.skypack.dev/three@0.128.0/build/three.module.js';
-import { OrbitControls } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/loaders/GLTFLoader.js';
+import {OrbitControls} from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/controls/OrbitControls.js';
+import {GLTFLoader} from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/loaders/GLTFLoader.js';
+
 
 
 // Setup
@@ -16,15 +16,15 @@ const geometry = new THREE.BoxGeometry(10, 10, 10);
 
 // Textures
 const losTexture = new THREE.TextureLoader().load('images/losAngeles.jpg');
-const laTexture = new THREE.TextureLoader().load('images/laLogo.jpg');
+const texTexture = new THREE.TextureLoader().load('images/laLogo.jpg');
 const skyTexture = new THREE.TextureLoader().load('images/night_sky.jpg');
-const texTexture = new THREE.TextureLoader().load('images/texture.jpg');
+const laTexture = new THREE.TextureLoader().load('images/texture.jpg');
 
 //set the color of the basic material in the object parameters `{}`
 
-const material = new THREE.MeshStandardMaterial( { color:0x6347FF, map:laTexture} );
+const material = new THREE.MeshStandardMaterial({color: 0x6347FF, map: laTexture});
 
-const cube = new THREE.Mesh( geometry, material );
+const cube = new THREE.Mesh(geometry, material);
 cube.position.z = -25;
 cube.position.x = -1;
 cube.rotation.x = 2;
@@ -32,19 +32,18 @@ cube.rotation.y = .8;
 
 
 const ico = new THREE.IcosahedronGeometry(15, 1);
-const icoMaterial = new THREE.MeshPhongMaterial({ color:0x33AADD, map:texTexture, shininess:75});
+const icoMaterial = new THREE.MeshPhongMaterial({color: 0x33AADD, map: skyTexture, shininess: 75});
 
 const icoMesh = new THREE.Mesh(ico, icoMaterial);
 // object.position.set ( x, y, z );
-icoMesh.position.set(80, -10, -65)
-// icoMesh.position.z= -20;
-// icoMesh.position.x= 30;
+icoMesh.position.set(80, -15, -65)
 
-const sphereGeo = new THREE.SphereGeometry( 20, 32, 16 );
-const sphereMaterial = new THREE.MeshBasicMaterial( { map:skyTexture } );
-const sphere = new THREE.Mesh( sphereGeo, sphereMaterial ); scene.add( sphere );
+const sphereGeo = new THREE.SphereGeometry(20, 32, 16);
+const sphereMaterial = new THREE.MeshBasicMaterial({map: texTexture});
+const sphere = new THREE.Mesh(sphereGeo, sphereMaterial);
+scene.add(sphere);
 // object.position.set ( x, y, z );
-sphere.position.set(-80,15,-45)
+sphere.position.set(-80, 5, -45)
 
 
 // Lights
@@ -81,7 +80,7 @@ camera.position.setY(10);
 renderer.render(scene, camera);
 
 function animate() {
-    requestAnimationFrame( animate );
+    requestAnimationFrame(animate);
     // slowly rotate the cube:
     cube.rotation.x += 0.02;
     cube.rotation.y += 0.02;
@@ -91,9 +90,8 @@ function animate() {
     sphere.rotation.x += -0.01
     sphere.rotation.z += -0.001
 
-    renderer.render( scene, camera );
+    renderer.render(scene, camera);
 }
 
 animate();
-
 
